1b2563fbeeb11f9deef22966656ae1676c8d02c3
Fucci Im In <benjaminakacha@gmail.com>
2024-09-17 12:12:30 -0700

    Added my own solution using Multer library

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const path = require('path');
 
 var app = express();
 
@@ -11,6 +13,28 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Set up storage using Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './filesUploaded'); // Set the destination folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Set the filename
+  }
+});
+
+// Initialize Multer with the defined storage
+const upload = multer({ storage: storage });
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  // Log information about the uploaded file
+  console.log(req.file); // This will log the file details
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 
